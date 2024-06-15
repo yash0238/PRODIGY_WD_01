9dea680d041774bc1665c7934820620bfda7ff5c
YashO238 <www.yyaasshh@gmail.com>
2024-06-15 14:01:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-/* script.js */
 const nav = document.getElementById('main-nav');
 const navToggle = document.getElementById('nav-toggle');
-
 navToggle.addEventListener('click', function() {
-  nav.classList.toggle('open'); /* Toggle open/closed state using class manipulation */
+  nav.classList.toggle('open');
 });
